5e7643f6182df299d9a380e634331e7a86354a47
John Doe <johndoe@example.com>
2026-01-06 21:02:13 -0300

    Modularity: Organizing the main code, splitting into proper parts

---
+++ venmo.py
@@ -0,0 +1,78 @@
+import re
+import uuid
+
+from exceptions import CreditCardException, PaymentException, UsernameException
+
+
+class Payment:
+    def __init__(self, amount, actor, target, note):
+        self.id = str(uuid.uuid4())
+        self.amount = float(amount)
+        self.actor = actor
+        self.target = target
+        self.note = note
+
+
+class User:
+    def __init__(self, username):
+        self.credit_card_number = None
+        self.balance = 0.0
+
+        if self._is_valid_username(username):
+            self.username = username
+        else:
+            raise UsernameException("Username not valid.")
+
+    def retrieve_feed(self):
+        # TODO: add code here
+        return []
+
+    def add_friend(self, new_friend):
+        # TODO: add code here
+        pass
+
+    def add_to_balance(self, amount):
+        self.balance += float(amount)
+
+    def add_credit_card(self, credit_card_number):
+        if self.credit_card_number is not None:
+            raise CreditCardException("Only one credit card per user!")
+
+        if self._is_valid_credit_card(credit_card_number):
+            self.credit_card_number = credit_card_number
+        else:
+            raise CreditCardException("Invalid credit card number.")
+
+    def pay(self, target, amount, note):
+        # TODO: add logic to pay with card or balance
+        pass
+
+    def pay_with_card(self, target, amount, note):
+        amount = float(amount)
+
+        if self.username == target.username:
+            raise PaymentException("User cannot pay themselves.")
+        elif amount <= 0.0:
+            raise PaymentException("Amount must be a non-negative number.")
+        elif self.credit_card_number is None:
+            raise PaymentException("Must have a credit card to make a payment.")
+
+        self._charge_credit_card(self.credit_card_number)
+        payment = Payment(amount, self, target, note)
+        target.add_to_balance(amount)
+
+        return payment
+
+    def pay_with_balance(self, target, amount, note):
+        # TODO: add code here
+        pass
+
+    def _is_valid_credit_card(self, credit_card_number):
+        return credit_card_number in ["4111111111111111", "4242424242424242"]
+
+    def _is_valid_username(self, username):
+        return re.match("^[A-Za-z0-9_\\-]{4,15}$", username)
+
+    def _charge_credit_card(self, credit_card_number):
+        # magic method that charges a credit card thru the card processor
+        pass
